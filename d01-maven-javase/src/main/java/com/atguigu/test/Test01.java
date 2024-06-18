4a206c8c0d16b52e6ae2883f1e52ebfec2ff52e7
chen-wei <2201313713@qq.com>
2024-06-18 17:18:55 +0800

    李四第一次修改代码

--- d01-maven-javase/src/main/java/com/atguigu/test/Test01.java
+++ d01-maven-javase/src/main/java/com/atguigu/test/Test01.java
@@ -14,5 +14,6 @@ public class Test01 {
         System.out.println("hello");
         System.out.println("hello");
         System.out.println("热修复.............");
+        System.out.println("李四修改了代码.............");
     }
 }

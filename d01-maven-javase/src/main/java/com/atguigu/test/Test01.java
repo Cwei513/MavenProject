c75791a177036c559f392a3f1795c474955c5095
chen-wei <2201313713@qq.com>
2024-06-18 16:49:27 +0800

    创建单元测试类

---
+++ d01-maven-javase/src/main/java/com/atguigu/test/Test01.java
@@ -0,0 +1,13 @@
+package com.atguigu.test;
+
+/**
+ * @Author : chen wei
+ * @Date : 2024/06/18 16:48
+ * @Version: 1.0
+ * @Description :
+ */
+public class Test01 {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}

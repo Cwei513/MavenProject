eba3b34e0a630c70cdb91c95341e58afbc1e41f2
chen-wei <2201313713@qq.com>
2024-06-18 16:50:04 +0800

    修改单元测试

--- d01-maven-javase/src/main/java/com/atguigu/test/Test01.java
+++ d01-maven-javase/src/main/java/com/atguigu/test/Test01.java
@@ -9,5 +9,9 @@ package com.atguigu.test;
 public class Test01 {
     public static void main(String[] args) {
         System.out.println("hello");
+        System.out.println("hello");
+        System.out.println("hello");
+        System.out.println("hello");
+        System.out.println("hello");
     }
 }
